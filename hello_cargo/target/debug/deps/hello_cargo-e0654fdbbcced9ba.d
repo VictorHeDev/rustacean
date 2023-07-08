01574c80a6e997fd4b1b676384634dc9e89cf6e3
Woodstock <victorhe97@gmail.com>
2023-07-08 12:53:47 -0400

    Remove hello cargo

--- hello_cargo/target/debug/deps/hello_cargo-e0654fdbbcced9ba.d
+++
@@ -1,5 +0,0 @@
-/Users/victorhe/Projects/rustacean/hello_cargo/target/debug/deps/hello_cargo-e0654fdbbcced9ba: src/main.rs
-
-/Users/victorhe/Projects/rustacean/hello_cargo/target/debug/deps/hello_cargo-e0654fdbbcced9ba.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/victorhe/Projects/rustacean/hello_cargo/target/debug/hello_cargo: /Users/victorhe/Projects/rustacean/hello_cargo/src/main.rs
